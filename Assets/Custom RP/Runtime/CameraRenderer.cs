a7007092abb491a875e0c7a4de76dce619870846
Fernando Lincoln <lincolncpp@gmail.com>
2022-05-26 22:54:45 -0300

    implement unsupported shader visualization

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -7,6 +7,17 @@ public class CameraRenderer {
 
 	private static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
+	private static ShaderTagId[] legacyShaderTagIds = {
+		new ShaderTagId("Always"),
+		new ShaderTagId("ForwardBase"),
+		new ShaderTagId("PrepassBase"),
+		new ShaderTagId("Vertex"),
+		new ShaderTagId("VertexLMRGBM"),
+		new ShaderTagId("VertexLM")
+	};
+
+	private static Material errorMaterial;
+
 	private ScriptableRenderContext context;
 	private Camera camera;
 
@@ -26,6 +37,7 @@ public class CameraRenderer {
 		}
 
 		Setup();
+		DrawUnsupportedShaders();
 		DrawVisibleGeometry();
 		Submit();
 	}
@@ -53,6 +65,27 @@ public class CameraRenderer {
 		drawingSettings.sortingSettings = sortingSettings;
 		filteringSettings.renderQueueRange = RenderQueueRange.transparent;
 
+		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+	}
+
+	private void DrawUnsupportedShaders() {
+		if (errorMaterial == null){
+			errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+		}
+
+
+		DrawingSettings drawingSettings = new DrawingSettings(
+			legacyShaderTagIds[0],
+			new SortingSettings(camera)
+		) {
+			overrideMaterial = errorMaterial
+		};
+
+		for(int i = 1;i < legacyShaderTagIds.Length; i++) {
+			drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+		}
+
+		FilteringSettings filteringSettings = FilteringSettings.defaultValue;
 		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
 	}
 
